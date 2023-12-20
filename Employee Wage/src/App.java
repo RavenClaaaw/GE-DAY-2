a06b8d64dfe129c713acc2b8baa7c92126851726
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-20 11:16:08 +0530

    Point Line Length

--- Employee Wage/src/App.java
+++ Employee Wage/src/App.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome To Comparision Computation: ");
+        
+    }
+
+}
+
+class Point{
+    double x, y;
+
+    Point(){}
+
+    Point(double aX, double aY){
+        this.x = aX;
+        this.y = aY;
+    }
+}
+
+class Line{
+    Point start = new Point();
+    Point end = new Point();
+
+    Line(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ENTER X-1: ");
+        start.x = scanner.nextDouble();
+
+        System.out.println("ENTER Y-1: ");
+        start.y = scanner.nextDouble();
+
+        System.out.println("ENTER X-2: ");
+        end.x = scanner.nextDouble();
+
+        System.out.println("ENTER Y-2: ");
+        end.y = scanner.nextDouble();
+
+        scanner.close();
+    }
+
+    double getLength(){
+        return Math.sqrt(Math.pow((start.x - end.x), 2) + Math.pow((start.y - end.y), 2));
     }
 }
